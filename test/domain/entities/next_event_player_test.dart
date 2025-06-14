6b372c99c03fad54ae169862875ae9aa363e35b6
Thierry Oliveira <thierry.ifsp@gmail.com>
2025-06-14 10:51:05 -0300

    feat: return the correct initials when there is a single name with a single char

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -38,28 +38,37 @@ class NextEventPlayer {
 
   static String _getInitials(String name) {
     final names = name.split(' ');
-    final firstNameInitial = names.first[0].toUpperCase();
-    final lastNameInitial = names.last[names.length == 1 ? 1 : 0].toUpperCase();
-    return '$firstNameInitial$lastNameInitial';
+    final firstChar = names.first[0];
+    final lastChar =
+        names.last.split('').elementAtOrNull(names.length == 1 ? 1 : 0) ?? '';
+    return '$firstChar$lastChar'.toUpperCase();
   }
-}
 
-void main() {
-  String initialsOf(String name) =>
-      NextEventPlayer(id: '', name: name, isConfirmed: true).initials;
+  void main() {
+    String initialsOf(String name) =>
+        NextEventPlayer(id: '', name: name, isConfirmed: true).initials;
 
-  test('should return the initial of the first and last names', () {
-    expect(initialsOf('Thierry Oliveira'), 'TO');
-    expect(initialsOf('Lorem Ipsum'), 'LI');
-    expect(initialsOf('Rogerio Mucki Ceni'), 'RC');
-  });
+    test('should return the initial of the first and last names', () {
+      expect(initialsOf('Thierry Oliveira'), 'TO');
+      expect(initialsOf('Lorem Ipsum'), 'LI');
+      expect(initialsOf('Rogerio Mucki Ceni'), 'RC');
+    });
 
-  test('should return the first two letters in case there is only a name', () {
-    expect(initialsOf('Thierry'), 'TH');
-  });
+    test(
+      'should return the first two letters in case there is only a name',
+      () {
+        expect(initialsOf('Thierry'), 'TH');
+      },
+    );
 
-  test('should convert to uppercase', () {
-    expect(initialsOf('thierry oliveira'), 'TO');
-    expect(initialsOf('thierry'), 'TH');
-  });
+    test('should return the single char when there is only a char as name', () {
+      expect(initialsOf('T'), 'T');
+      expect(initialsOf('t'), 'T');
+    });
+
+    test('should convert to uppercase', () {
+      expect(initialsOf('thierry oliveira'), 'TO');
+      expect(initialsOf('thierry'), 'TH');
+    });
+  }
 }
