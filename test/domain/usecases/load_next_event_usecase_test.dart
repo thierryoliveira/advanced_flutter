7c4572a52d83e2c6ec83c673d0f741ed1672a343
Thierry Oliveira <thierry.ifsp@gmail.com>
2025-06-14 20:54:00 -0300

    feat: apply Dependency Inversion principle to the Next Event repo

--- test/domain/usecases/load_next_event_usecase_test.dart
+++ test/domain/usecases/load_next_event_usecase_test.dart
@@ -12,9 +12,14 @@ class LoadNextEventUseCase {
   }
 }
 
-class LoadNextEventRepository {
+abstract class LoadNextEventRepository {
+  Future<void> loadNextEvent({required String groupId});
+}
+
+class LoadNextEventMockRepository implements LoadNextEventRepository {
   String? groupId;
 
+  @override
   Future<void> loadNextEvent({required String groupId}) async {
     this.groupId = groupId;
   }
@@ -22,7 +27,7 @@ class LoadNextEventRepository {
 
 void main() {
   test('should load event data from a repository', () async {
-    final repository = LoadNextEventRepository();
+    final repository = LoadNextEventMockRepository();
     final sut = LoadNextEventUseCase(loadNextEventRepository: repository);
     final groupId = Random().nextInt(50000).toString();
     await sut(groupId: groupId);
