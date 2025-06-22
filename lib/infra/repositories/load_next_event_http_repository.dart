db2424cbe0d37c1f39d8b426fe1f4783237bf8eb
Thierry Oliveira <thierry.ifsp@gmail.com>
2025-06-22 11:34:34 -0300

    chore: make [LoadNextEventHttpRepository] implements [LoadNextEventRepository]

--- lib/infra/repositories/load_next_event_http_repository.dart
+++ lib/infra/repositories/load_next_event_http_repository.dart
@@ -1,7 +1,8 @@
 import 'package:advanced_flutter/domain/entities/next_event.dart';
+import 'package:advanced_flutter/domain/repositories/load_next_event_repository.dart';
 import 'package:http/http.dart';
 
-class LoadNextEventHttpRepository {
+class LoadNextEventHttpRepository implements LoadNextEventRepository {
   final Client client;
   final String url;
   final Map<String, String>? headers;
@@ -12,6 +13,7 @@ class LoadNextEventHttpRepository {
     this.headers,
   });
 
+  @override
   Future<NextEvent> loadNextEvent({required String groupId}) async {
     final urlWithParams = url.replaceFirst(':groupId', groupId);
     final result = await client.get(Uri.parse(urlWithParams), headers: headers);
