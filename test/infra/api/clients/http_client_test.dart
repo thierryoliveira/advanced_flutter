e0310f7bf0a773916da7b0376b6be8fc05c95ace
Thierry Oliveira <thierry.ifsp@gmail.com>
2025-06-22 21:51:25 -0300

    feat: ensure HttpClient request with correct headers for GET calls

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -9,8 +9,14 @@ class HttpClient {
 
   HttpClient({required this.client});
 
-  Future<void> get({required String url}) async {
-    await client.get(Uri.parse(url));
+  Future<void> get({required String url, Map<String, String>? headers}) async {
+    final requestHeaders = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+      ...?headers,
+    };
+
+    await client.get(Uri.parse(url), headers: requestHeaders);
   }
 }
 
@@ -23,23 +29,57 @@ void main() {
     registerFallbackValue(Uri());
   });
 
+  When<Future<Response>> mockClient() =>
+      when(() => client.get(any(), headers: any(named: 'headers')));
+
   setUp(() {
     client = ClientSpy();
     sut = HttpClient(client: client);
     url = anyString();
 
-    when(() => client.get(any())).thenAnswer((_) async => Response('', 200));
+    mockClient().thenAnswer((_) async => Response('', 200));
   });
 
   group('GET method:', () {
     test('should request using the correct method', () async {
       await sut.get(url: '');
-      verify(() => client.get(any())).called(1);
+      verify(() => client.get(any(), headers: any(named: 'headers'))).called(1);
     });
 
     test('should request with the correct URL', () async {
       await sut.get(url: url);
-      verify(() => client.get(Uri.parse(url)));
+      verify(() => client.get(Uri.parse(url), headers: any(named: 'headers')));
     });
+
+    test('should request with default headers when none is provided', () async {
+      await sut.get(url: url);
+      verify(
+        () => client.get(
+          any(),
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+          },
+        ),
+      ).called(1);
+    });
+
+    test(
+      'should request with the correct headers appending the default when custom headers are provided',
+      () async {
+        await sut.get(url: url, headers: {'h1': 'header1', 'h2': 'header2'});
+        verify(
+          () => client.get(
+            any(),
+            headers: {
+              'content-type': 'application/json',
+              'accept': 'application/json',
+              'h1': 'header1',
+              'h2': 'header2',
+            },
+          ),
+        ).called(1);
+      },
+    );
   });
 }
