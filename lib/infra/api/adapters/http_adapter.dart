ee3286a7a6cd39b40d13e2190261a325bda481ab
Thierry Oliveira <thierry.ifsp@gmail.com>
2025-06-24 10:43:24 -0300

    feat: makes HttpAdatper implements HttpGetClient

--- lib/infra/api/adapters/http_adapter.dart
+++ lib/infra/api/adapters/http_adapter.dart
@@ -1,15 +1,17 @@
 import 'dart:convert';
 
 import 'package:advanced_flutter/domain/entities/domain_error.dart';
+import 'package:advanced_flutter/infra/api/clients/http_get_client.dart';
 import 'package:advanced_flutter/infra/types/json.dart';
 import 'package:dartx/dartx.dart';
 import 'package:http/http.dart';
 
-class HttpAdapter {
+class HttpAdapter implements HttpGetClient {
   final Client client;
 
   HttpAdapter({required this.client});
 
+  @override
   Future<T?> get<T>({
     required String url,
     Map<String, String>? headers,
