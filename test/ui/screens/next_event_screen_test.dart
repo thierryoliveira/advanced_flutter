c0de31d30f9ad1385f49a77ea1fcdb678172051d
Thierry Oliveira <thierry.ifsp@gmail.com>
2025-06-25 11:17:37 -0300

    feat: ensure NextEventScreen hides the spinner on loaded

--- test/ui/screens/next_event_screen_test.dart
+++ test/ui/screens/next_event_screen_test.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -28,12 +29,25 @@ class _NextEventScreenState extends State<NextEventScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: CircularProgressIndicator());
+    return Scaffold(
+      body: StreamBuilder(
+        stream: widget.presenter.nextEventStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.active) {
+            return CircularProgressIndicator();
+          }
+          return SizedBox.shrink();
+        },
+      ),
+    );
   }
 }
 
 abstract class NextEventPresenter {
+  Stream get nextEventStream;
+
   void loadNextEvent({required String groupId});
+  void emitNextEvent();
 }
 
 final class NextEventPresenterSpy extends Mock implements NextEventPresenter {}
@@ -42,13 +56,21 @@ void main() {
   late Widget sut;
   late NextEventPresenter presenter;
   late String groupId;
+  late StreamController<String> controller;
 
   setUp(() {
     presenter = NextEventPresenterSpy();
     groupId = anyString();
+    controller = StreamController<String>();
     sut = MaterialApp(
       home: NextEventScreen(presenter: presenter, groupId: groupId),
     );
+
+    when(() => presenter.nextEventStream).thenAnswer((_) => controller.stream);
+  });
+
+  tearDown(() {
+    controller.close();
   });
 
   testWidgets('should load event data on screen inits', (tester) async {
@@ -63,5 +85,18 @@ void main() {
     await tester.pumpWidget(sut);
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('should hide the spinner when the data is loaded successfully', (
+    tester,
+  ) async {
+    await tester.pumpWidget(sut);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    controller.add('event');
+    verify(() => presenter.loadNextEvent(groupId: groupId)).called(1);
+
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }
